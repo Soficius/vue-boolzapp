c297e6349b13fff3e061e7c1b7f99f693450c8fc
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-14 21:40:29 +0200

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        contacts: [
+            {nome: 'Michele', photo:"img/avatar_1.jpg" },
+            {nome: 'Fabio', photo:"img/avatar_2.jpg" },
+            {nome: 'Samuele', photo:"img/avatar_3.jpg" },
+            {nome: 'Luisa', photo:"img/avatar_4.jpg" },
+        ]
+    }
+})
